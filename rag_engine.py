d99276d217644d97e78259b243d50d7c9d5de4e6
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-04-14 19:13:55 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -1,9 +1,30 @@
+import chromadb
+from sentence_transformers import SentenceTransformer
+
+
 class RAGEngine:
 
     def __init__(self):
-        pass
+        self.embedder = SentenceTransformer(
+            "all-MiniLM-L6-v2"
+        )
 
-    def search(self, query):
-        return [
-            "Relevant UDCPR clause text will come here."
-        ]
+        self.client = chromadb.PersistentClient(
+            path="./vector_db"
+        )
+
+        self.collection = self.client.get_collection(
+            "udcpr"
+        )
+
+    def search(self, query, top_k=3):
+        embedding = self.embedder.encode(
+            [query]
+        ).tolist()
+
+        result = self.collection.query(
+            query_embeddings=embedding,
+            n_results=top_k
+        )
+
+        return result["documents"][0]
